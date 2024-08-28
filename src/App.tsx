53b9f1370574c293962917b953b035b4a18bfcf0
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-08-28 16:02:29 +0600

    🚀 Setup: VSCode Configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from "@/assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="min-h-svh grid place-content-center gap-1">
-      <div className='flex items-center gap-2 justify-center'>
+      <div className="flex items-center gap-2 justify-center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="" alt="Vite logo" />
         </a>
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import App from '@/App.tsx'
-import '@/index.css'
+import App from "@/App.tsx";
+import "@/index.css";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+if (!root) throw new Error("No root element found");
+
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
