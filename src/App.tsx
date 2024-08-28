d0c9a15fa3d8ab3242799b6b08bc0738a39298b1
Talha Ibne Mahmud <71333752+talhaibnmahmud@users.noreply.github.com>
2024-08-28 15:56:59 +0600

    🚀 Setup: Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 
-import '@/App.css'
 import reactLogo from '@/assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -8,13 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className="min-h-svh grid place-content-center gap-1">
+      <div className='flex items-center gap-2 justify-center'>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
@@ -29,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
